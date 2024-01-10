35af92b61b2915ec8a5c4836128bfb7c7cac8119
iscii <issac.zheng9869@gmail.com>
2024-01-10 11:40:16 -0500

    remove unused package, fix scroll to #about

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,10 @@ import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import ScrollToAnchor from "./components/utilities/ScrollToAnchor";
 
-import { useScrollPercentage } from "react-scroll-percentage";
-
 function App() {
-    const [ref, percentage] = useScrollPercentage({ threshold: 0 });
-
 	return (
         // can create a new div for format if i ever want to use empty space. can move into a component that defines structure of page and wraps other components.
-        <div className="grid grid-cols-12 w-full h-full" ref={ref}>
+        <div className="grid grid-cols-12 w-full h-full">
             <div className="col-start-3 col-span-8">
                 <ContextProvider>
                     <Nav />
--- src/components/utilities/ScrollToAnchor.jsx
+++ src/components/utilities/ScrollToAnchor.jsx
@@ -2,25 +2,26 @@ import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function ScrollToAnchor() {
-  const location = useLocation();
-  const lastHash = useRef('');
+	const location = useLocation();
+	const lastHash = useRef('');
 
-  // listen to location change using useEffect with location as dependency
-  // https://dev.to/mindactuate/scroll-to-anchor-element-with-react-router-v6-38op
-  useEffect(() => {
-    // Modify this for #about
-    if (location.hash) {
-      lastHash.current = location.hash.slice(1); // save hash for further use after navigation
-    }
+	// listen to location change using useEffect with location as dependency
+	// https://dev.to/mindactuate/scroll-to-anchor-element-with-react-router-v6-38op
+	useEffect(() => {
+		if (location.hash) {
+		lastHash.current = location.hash.slice(1); // save hash for further use after navigation
+		}
 
-    if (lastHash.current && document.getElementById(lastHash.current)) {
-        document.getElementById(lastHash.current)
-               ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        lastHash.current = '';
-    }
-  }, [location]);
+		if (lastHash?.current === 'about') {
+			window.scrollTo({ top: 0, behavior: 'smooth' });
+		} else if (lastHash.current && document.getElementById(lastHash.current)) {
+			document.getElementById(lastHash.current)
+				?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+			lastHash.current = '';
+		}
+	}, [location]);
 
-  return null;
+	return null;
 }
 
 export default ScrollToAnchor;
