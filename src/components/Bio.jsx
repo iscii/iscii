65a0224a7728f5ffbf4bd92c3d02d97809e90e41
iscii <issac.zheng9869@gmail.com>
2024-01-12 14:59:04 -0500

    some spacing

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,12 +7,15 @@ function Bio(props) {
 	return (
 		<div id="about" className='sticky top-24 pt-12 px-24 z-0 flex flex-row justify-between' style={{ opacity: bioOpacity }}>
             <div className='relative w-8/12'>
-                Hi! I'm Issac. 
-                <br/> 
-                I currently attend Stevens Institute of Technology and study Computer Science. 
-                <br/>
-                I enjoy music, exploring, and the little things; related to my position, problem solving and a solid challenge. I'm looking for work in a warm, 
-                ever-improving environment driven by a shared goal where I could palpably reap the rewards of my years of work and make a difference.
+                <div className='mb-4'>
+                    Hi! I'm Issac. 
+                </div>
+                <div>
+                    I currently attend Stevens Institute of Technology and study Computer Science. 
+                    <br/>
+                    I enjoy music, exploring, and the little things; related to my position, problem solving and a solid challenge. I'm looking for work in a warm, 
+                    ever-improving environment driven by a shared goal where I could palpably reap the rewards of my years of work and make a difference.
+                </div>
                 <br/>
                 <div className='absolute bottom-0'>
                     Wanna reach out to me? <FaRegHandPointRight className='inline'/> <a href='mailto:izheng@stevens.edu'><FaRegEnvelope className='inline'/></a>
