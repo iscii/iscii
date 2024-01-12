117405854961afd2ec9da5bbde22f5c0e0508dbb
iscii <issac.zheng9869@gmail.com>
2024-01-11 23:20:47 -0500

    added projects subtabs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,6 +11,7 @@ function Nav() {
 			<div className="grid grid-rows-3 w-1/2 h-full text-sm">
 				<div className='grid row-start-2 row-span-1 grid-cols-6'>
 					<div className='col-start-1 col-span-1 flex justify-center items-center relative'>
+						{/* TODO: componentize these tabs, then make it functional. */}
 						<div className='before:absolute before:-bottom-[0.3rem] before:left-[45%] before:w-2 before:h-2 before:rotate-45 before:bg-white before:dark:bg-slate-800 after:absolute after:-bottom-[0.3rem] after:left-[45%] after:w-2 after:h-2 after:rotate-45 after:border-b-2 after:border-r-2'>
 							<Link to='/#about' className=''>About</Link></div>
 						</div>
@@ -21,6 +22,12 @@ function Nav() {
 					<div className='col-start-1 flex justify-center items-center border-t-2'><Link to='/#featured'>Featured</Link></div>
 					<div className='col-start-2 flex justify-center items-center border-t-2'><Link to='/#experience'>Experience</Link></div>
 				</div>
+				{/* <div className='grid row-start-3 row-span-1 grid-cols-6'>
+					<div className='col-start-1 flex justify-center items-center border-t-2'><Link to='/#websites'>Websites</Link></div>
+					<div className='col-start-2 flex justify-center items-center border-t-2'><Link to='/#games'>Games</Link></div>
+					<div className='col-start-3 flex justify-center items-center border-t-2'><Link to='/#programs'>Programs</Link></div>
+					<div className='col-start-4 flex justify-center items-center border-t-2'><Link to='/#webgames'>Web Games</Link></div>
+				</div> */}
 			</div>
 			<div className='flex flex-row justify-end'>
 				<a href='https://github.com/iscii' target="_blank" rel="noopener noreferrer"><FaGithub size={35}/></a>
