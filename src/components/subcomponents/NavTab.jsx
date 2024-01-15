1ece7172d71a519baa747614b8142dfda8d14d57
iscii <issac.zheng9869@gmail.com>
2024-01-14 19:38:09 -0500

    made arrow point upwards to avoid confusion -> instead of on subtab, more clear that youre on navtab

--- src/components/subcomponents/NavTab.jsx
+++ src/components/subcomponents/NavTab.jsx
@@ -5,7 +5,7 @@ function NavTab(props) {
     const [openTab, setOpenTab] = openTabState || [null, null];
     return (
         <div className={`col-start-${col} col-span-1 flex justify-center items-center relative`}>
-            <div className={(openTab == col) ? 'before:absolute before:-bottom-[0.3rem] before:left-[45%] before:w-2 before:h-2 before:rotate-45 before:bg-white before:dark:bg-slate-800 after:absolute after:-bottom-[0.3rem] after:left-[45%] after:w-2 after:h-2 after:rotate-45 after:border-b-2 after:border-r-2' : ''}>
+            <div className={(openTab == col) ? 'before:absolute before:-bottom-[0.275rem] before:left-[45%] before:w-2 before:h-2 before:rotate-45 before:bg-white before:dark:bg-slate-800 after:absolute after:-bottom-[0.275rem] after:left-[45%] after:w-2 after:h-2 after:rotate-45 after:border-t-2 after:border-l-2' : ''}>
                 <Link to={link} target={target} onClick={() => {if (openTab) setOpenTab(col)}}>{props.children}</Link>
             </div>
         </div>
--- src/components/subcomponents/SubTab.jsx
+++ src/components/subcomponents/SubTab.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function SubTab(props) {
     const {col, link, target} = props;
     return (
-        <div className={`col-start-${col} flex justify-center items-end border-t-2`}><Link to={link} target={target}>{props.children}</Link></div>
+        <div className={`col-start-${col} flex justify-center items-center border-t-2`}><Link to={link} target={target}>{props.children}</Link></div>
     );
 }
 
