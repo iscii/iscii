60cf05452de8420e6cb77d04b91fe7e299a31d95
Issac <issac.zheng9869@gmail.com>
2024-01-15 22:37:00 -0500

    create css class for gradient bc tailwind uses image which doesnt translate well on themeing and dark reader

--- src/components/subcomponents/Header.jsx
+++ src/components/subcomponents/Header.jsx
@@ -3,7 +3,7 @@ function Header(props) {
 
 	return (
         // see if this can be done with normal css (make a replacement class)
-        <h1 className={`py-10 sticky top-20 z-20 text-xl ${opaque ? 'relative z-10 bg-white' : 'bg-gradient-to-t from-white to-transparent'}`}> 
+        <h1 className={`py-10 sticky top-20 z-20 text-xl ${opaque ? 'relative z-10 bg-white' : 'header-fade'}`}> 
             {props.children}
         </h1>
 	);
