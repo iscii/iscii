4722cca965868a60edd38d35ef61a42c132b5891
Issac <issac.zheng9869@gmail.com>
2024-01-16 16:48:02 -0500

    more mobile formatting and updates

--- src/components/subcomponents/Header.jsx
+++ src/components/subcomponents/Header.jsx
@@ -2,7 +2,7 @@ function Header(props) {
     const {opaque} = props;
 
 	return (
-        <h1 className={`py-10 sticky top-0 z-20 text-xl md:top-20 ${opaque ? 'relative z-10 bg-white' : 'header-fade'}`}> 
+        <h1 className={`py-5 sticky top-0 z-20 text-xl md:top-20 ${opaque ? 'relative z-10 bg-white' : 'header-fade'}`}> 
             {props.children}
         </h1>
 	);
--- src/components/subcomponents/ProjectCard.jsx
+++ src/components/subcomponents/ProjectCard.jsx
@@ -4,20 +4,22 @@ function ProjectCard(props) {
     const {name, desc, image, link, repo, category} = props;
 
 	return (
-		<div className='relative flex flex-col md:flex-row justify-between py-10 min-h-60 border-b-2 last-of-type:border-0'>
-            <h3 className="md:hidden">{name}</h3>
-            <a href={link} className='my-auto' target='_blank' rel='noopener noreferrer'>
-                <img src={`/images/${category}/${image}`} width="400" alt={name} title={name}/>
+		<div className='relative flex flex-col md:flex-row justify-start py-10 min-h-60 h-auto md:h-96 border-b-2 last-of-type:border-0'>
+            <h3 className="md:hidden text-xl mb-5">{name}</h3>
+            <a href={link} className='md:my-auto' target='_blank' rel='noopener noreferrer'>
+                <img src={`/images/${category}/${image}`} width={350} alt={name} title={name}/>
             </a>
-            <div className='md:w-8/12 md:pl-6 md:mt-0 mt-5'>
-                <h3 className="mb-4 hidden md:inline-block">{name}</h3>
-                <p>{desc}</p>
-                <div className="md:absolute md:bottom-5">
+            <div className='md:w-8/12 md:ml-12 md:mt-0 flex flex-col justify-between'>
+                <div className="my-5">
+                    <h3 className="mb-4 hidden md:inline-block">{name}</h3>
+                    <p>{desc}</p>
+                </div>
+                <div>
                     {   link &&
-                        <a className="pr-4 inline-flex items-center" href={link} target='_blank' rel='noopener noreferrer'>Link <FaExternalLinkAlt className="inline ml-2"/></a>
+                        <a className="pr-4 text-xl md:text-base inline-flex items-center" href={link} target='_blank' rel='noopener noreferrer'>Link <FaExternalLinkAlt className="inline ml-2"/></a>
                     }
                     {   repo &&
-                        <a className="inline-flex items-center" href={repo} target='_blank' rel='noopener noreferrer'>Repo <FaExternalLinkAlt className="inline ml-2"/></a>
+                        <a className="text-xl md:text-base inline-flex items-center" href={repo} target='_blank' rel='noopener noreferrer'>Repo <FaExternalLinkAlt className="inline ml-2"/></a>
                     }
                 </div>
             </div>
